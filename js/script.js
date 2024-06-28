8aff1d07111d5449e05e24c3525267f752ff3411
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-06-28 15:19:56 +0200

    Price variables declared

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
 let chilometri = prompt("Inserisci il numero di chilometri da percorrere:");
 let eta = prompt("Inserisci l'età del passeggero:");
+let prezzoPerKm = 0.21;
+let prezzoTotale = chilometri * prezzoPerKm;
