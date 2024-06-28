6a94092edfb4c61812030c8dccfd2a61c70f1b6f
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-06-28 15:20:53 +0200

    Condition on price declared

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ let chilometri = prompt("Inserisci il numero di chilometri da percorrere:");
 let eta = prompt("Inserisci l'età del passeggero:");
 let prezzoPerKm = 0.21;
 let prezzoTotale = chilometri * prezzoPerKm;
+if (eta < 18) {
+    prezzoTotale *= 0.8;
+} else if (eta > 65) {
+    prezzoTotale *= 0.6;
+}
